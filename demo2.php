ed7ea340637715c0a9bd6641029d74a7b6379f25
Jimmy <18838520606@163.com>
2018-12-15 16:01:41 +0800

    changed a bit

--- demo2.php
+++ demo2.php
@@ -48,6 +48,77 @@ class Gathering{
     }
 }
 
-$myform = new Gathering('template');
+//$myform = new Gathering('template');
+
+//The Gathering Class is meant to say
+
+//下面是一些魔术方法
+class Test{
+    private $valOne;
+    private $valTwo;
+    function __construct($a, $b)
+    {
+        if (isset($a) && isset($b)){
+            $this->valOne = $a;
+            $this->valTwo = $b;
+            return true;
+        }else{
+            return false;
+        }
+    }
+    /*
+     * 声明魔术方法需要两个参数，直接为私有属性赋值时自动调用，并可以屏蔽一些非法赋值
+     * @param string $propertyName
+     * @param mixed $propertyValue
+     */
+    function __set($name, $value)
+    {
+        // TODO: Implement __set() method.
+        if (isset($name)){
+            if ($name == 'valOne'){
+                $this->valOne = strval($value).'suffix';
+            }
+            if ($name == 'valTwo'){
+                $this->valTwo ='prefix'. strval($value);
+            }
+//            $this->$name = $value;
+            return true;
+        }else{
+            return false;
+        }
+    }
+
+    //关于magicmethod要注意:
+    //Warning: The magic method __get() must have public visibility and cannot be static
+    function __get($name)
+    {
+        // TODO: Implement __get() method.
+        return $this->$name;
+    }
+
+    function __isset($name)
+    {
+        // TODO: Implement __isset() method.
+        if ($name == 'valTwo'){
+            return !isset($this->$name);
+        }else{
+            return isset($this->$name);
+        }
+    }
+
+    function reply(){
+        echo $this->valTwo;
+        echo $this->valOne;
+    }
+}
+
+$myTest = new Test(1,1);
+$myTest->valOne =23333;
+echo $myTest->valOne;
+//当不存在get魔术方法时，会有报错:Cannot access private property Test::$valOne
+//当引用了set或者get魔术方法时，会看上去像没有封装过一样
+//set有一点好处，它可以屏蔽非法值
+var_dump(isset($myTest->valTwo));
+$myTest->reply();
 
 
